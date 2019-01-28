0a9775008fb75d111dc05dfa8ddc3df8766c9198
Rico-HBChen <35827440+Rico-HBChen@users.noreply.github.com>
2019-01-28 19:55:17 +0800

    Update face_test.py

---
+++ face_test.py
@@ -0,0 +1,9 @@
+import cv2
+filename='img/face_test.jpg'
+
+def detect(filename):
+    face_cascade=cv2.CascadeClassifier('/cascades/haarcascade_frontalface_default.xml')
+
+img=cv2.imread(filename)
+gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+faces=face_cascade.detectMultiScale(gray,1.3,5)
